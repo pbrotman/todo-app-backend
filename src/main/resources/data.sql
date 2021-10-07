d9fcedcd71f39cb8f85081238705f1abc70ff179
PARKER BROTMAN <pb1925a@student.american.edu>
2021-10-07 15:32:50 -0400

    Implemented JPA/Hibernate

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values(10001, 'parker', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10002, 'parker', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10003, 'parker', 'Learn Microservices', sysdate(), false);
